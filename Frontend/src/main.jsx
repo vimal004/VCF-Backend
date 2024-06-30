6f508bb12266239530e05a803a23e1a7e84279f7
vimal004 <2004.vimal@gmail.com>
2024-06-30 12:22:30 +0530

    15

--- Frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
