6b18b7f4d2f2335ba70f29fea99401601f11adc6
vimal004 <2004.vimal@gmail.com>
2024-07-08 16:44:00 +0530

    fixed bug

--- backend/Customer.js
+++ backend/Customer.js
@@ -78,15 +78,20 @@ customerRouter.get("/", (req, res) => {
 // Update a customer by id
 customerRouter.put("/customer", async (req, res) => {
   try {
+    // Filter out undefined fields from the request body
+    const updateData = Object.keys(req.body).reduce((acc, key) => {
+      if (req.body[key] !== undefined) {
+        acc[key] = req.body[key];
+      }
+      return acc;
+    }, {});
+
     const customer = await Customer.findOneAndUpdate(
       { id: req.body.id },
-      {
-        name: req.body.name,
-        address: req.body.address,
-        group: req.body.group,
-        phno: req.body.phno,
-      }
+      updateData,
+      { new: true } // This option returns the updated document
     );
+
     if (customer) {
       res.status(200).json({
         message: "Customer updated successfully",
